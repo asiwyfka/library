2faee552d9d9d148f82a900cee294ed88889be04
Viktor Avdeev <v.avdeev@mail.astondevs.ru>
2025-02-18 17:54:44 +0300

    Создание liquibase, скриптов для наполнения таблиц, конфиг и докер файла для приложения, также докер-компоуз файла.

---
+++ src/main/resources/db/changelog/changeset/insert-users.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (first_name, last_name, birth_date, date_registration, role)
+VALUES
+    ('John', 'Doe', '1990-04-15', '2023-01-10 10:15:00', 'READER'),
+    ('Emily', 'Johnson', '1995-07-25', '2023-02-14 09:45:00', 'READER'),
+    ('Michael', 'Smith', '1988-11-05', '2022-09-18 14:00:00', 'READER'),
+    ('Olivia', 'Brown', '2001-02-12', '2023-04-22 11:10:00', 'READER'),
+    ('William', 'Jones', '1993-05-30', '2023-03-01 08:30:00', 'READER'),
+    ('Sophia', 'Garcia', '1998-06-18', '2022-12-12 16:45:00', 'READER'),
+    ('James', 'Martinez', '1985-09-21', '2021-10-10 15:20:00', 'READER'),
+    ('Isabella', 'Miller', '1992-03-14', '2023-05-05 09:50:00', 'READER'),
+    ('Alexander', 'Davis', '1999-12-28', '2022-06-25 14:30:00', 'READER'),
+    ('Mia', 'Lopez', '2003-08-17', '2022-11-01 12:20:00', 'READER'),
+    ('Ethan', 'Wilson', '1997-04-04', '2023-07-19 10:40:00', 'READER'),
+    ('Ava', 'Anderson', '2000-10-10', '2021-03-03 17:30:00', 'READER'),
+    ('Benjamin', 'Thomas', '1983-02-07', '2023-01-29 18:15:00', 'READER'),
+    ('Charlotte', 'Moore', '1996-01-22', '2020-08-18 14:00:00', 'READER'),
+    ('Daniel', 'Taylor', '1989-06-11', '2022-04-04 13:05:00', 'READER');
